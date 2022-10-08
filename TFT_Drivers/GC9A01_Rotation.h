a1303db5428c78b87eb1648d1673c46cc5b6048b
9cats <58725483+9cats@users.noreply.github.com>
2022-10-08 16:50:48 +0800

    Fix GC9A01 drivers

--- TFT_Drivers/GC9A01_Rotation.h
+++ TFT_Drivers/GC9A01_Rotation.h
@@ -1,28 +1,45 @@
 
 // This is the command sequence that rotates the GC9A01 driver coordinate frame
 
-  rotation = m % 4;
+rotation = m % 4;
 
-  writecommand(TFT_MADCTL);
-  switch (rotation) {
-    case 0: // Portrait
-      writedata(TFT_MAD_BGR);
-      _width  = _init_width;
-      _height = _init_height;
-      break;
-    case 1: // Landscape (Portrait + 90)
-      writedata(TFT_MAD_MX | TFT_MAD_MV | TFT_MAD_BGR);
-      _width  = _init_height;
-      _height = _init_width;
-      break;
-    case 2: // Inverter portrait
-      writedata(TFT_MAD_MX | TFT_MAD_MY | TFT_MAD_BGR);
-      _width  = _init_width;
-      _height = _init_height;
-      break;
-    case 3: // Inverted landscape
-      writedata(TFT_MAD_MV | TFT_MAD_MY | TFT_MAD_BGR);
-      _width  = _init_height;
-      _height = _init_width;
-      break;
-  }
+writecommand(TFT_MADCTL);
+switch (rotation)
+{
+case 0: // Portrait
+#ifdef CGRAM_OFFSET
+  colstart = 2;
+  rowstart = 1;
+#endif
+  writedata(TFT_MAD_BGR);
+  _width = _init_width;
+  _height = _init_height;
+  break;
+case 1: // Landscape (Portrait + 90)
+#ifdef CGRAM_OFFSET
+  colstart = 1;
+  rowstart = 2;
+#endif
+  writedata(TFT_MAD_MX | TFT_MAD_MV | TFT_MAD_BGR);
+  _width = _init_height;
+  _height = _init_width;
+  break;
+case 2: // Inverter portrait
+#ifdef CGRAM_OFFSET
+  colstart = 2;
+  rowstart = 1;
+#endif
+  writedata(TFT_MAD_MX | TFT_MAD_MY | TFT_MAD_BGR);
+  _width = _init_width;
+  _height = _init_height;
+  break;
+case 3: // Inverted landscape
+#ifdef CGRAM_OFFSET
+  colstart = 1;
+  rowstart = 2;
+#endif
+  writedata(TFT_MAD_MV | TFT_MAD_MY | TFT_MAD_BGR);
+  _width = _init_height;
+  _height = _init_width;
+  break;
+}
